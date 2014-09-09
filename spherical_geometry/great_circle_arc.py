Fix area calculation. No longer use cutlines, but use a representation of disjoint closed polygons.

spherical_geometry/great_circle_arc.py
@@ -31,7 +31,7 @@ else:
 
 
 
-__all__ = ['angle', 'intersection', 'intersects', 'intersects_point', 
+__all__ = ['angle', 'intersection', 'intersects', 'intersects_point',
            'length', 'midpoint', 'interpolate']
 
 
@@ -284,8 +284,6 @@ def angle(A, B, C, degrees=True):
     """
     Returns the angle at *B* between *AB* and *BC*.
 
-    This always returns the shortest angle < π.
-
     Parameters
     ----------
     A, B, C : (*x*, *y*, *z*) triples or Nx3 arrays of triples
@@ -298,7 +296,7 @@ def angle(A, B, C, degrees=True):
     Returns
     -------
     angle : float or array of floats
-        The angle at *B* between *AB* and *BC*.
+        The angle at *B* between *AB* and *BC*, in range 0 to 2π.
 
     References
     ----------
@@ -316,11 +314,16 @@ def angle(A, B, C, degrees=True):
     ABX = _cross_and_normalize(B, ABX)
     BCX = _fast_cross(C, B)
     BCX = _cross_and_normalize(B, BCX)
+    X = _cross_and_normalize(ABX, BCX)
+    diff = inner1d(B, X)
+    inner = inner1d(ABX, BCX)
     with np.errstate(invalid='ignore'):
-        angle = np.arccos(inner1d(ABX, BCX))
+        angle = np.arccos(inner)
+    angle = np.where(diff < 0.0, (2.0 * np.pi) - angle, angle)
 
     if degrees:
         angle = np.rad2deg(angle)
+
     return angle
 
 

spherical_geometry/tests/test_union.py
@@ -56,8 +56,6 @@ class union_test:
                     permutation)
                 unions.append(union)
                 union_area = union.area()
-                print(union._points)
-                print(permutation[0]._points)
 
                 if GRAPH_MODE:
                     fig = plt.figure()
@@ -74,10 +72,11 @@ class union_test:
                     plt.savefig(filename)
                     fig.clear()
 
-                print(union_area, areas)
                 assert np.all(union_area * 1.1 >= areas)
 
-            lengths = np.array([len(x._points) for x in unions])
+            lengths = np.array([
+                np.sum(len(x._points) for x in y.iter_polygons_flat())
+                for y in unions])
             assert np.all(lengths == [lengths[0]])
             areas = np.array([x.area() for x in unions])
             assert_array_almost_equal(areas, areas[0], 1)
@@ -188,7 +187,8 @@ def test_union_empty():
 
     p2 = p.union(polygon.SphericalPolygon([]))
 
-    assert_array_almost_equal(p2._points, p._points)
+    assert len(p2.polygons) == 1
+    assert_array_almost_equal(p2.polygons[0].points, p.polygons[0].points)
 
 
 def test_difficult_unions():