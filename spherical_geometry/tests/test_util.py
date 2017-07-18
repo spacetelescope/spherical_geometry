1ff163f9d77134fdce73d430cd3902947458b7b6
Bernie Simon <bsimon@stsci.edu>
2017-07-18 15:53:29 -0400

    Make numpy.linspace step count an integer
    The numpy function linspace computes an evenly spaced array of
    values. In the current version of numpy it checks if the number of
    steps is an integer.
    Method from_code computes a spherical polygon representing the
    intersection of a cone with the unit sphere. The number of steps used
    in computing the pyramid approximating the cone had a default value
    that was a float. That value was used when calling linspace, resulting
    in an error. So the value has been changed from a float to an integer.
    Get_point_set is a utility function used in testing. It covers an
    ra/dec box in a grid of points. The number of points is less at higher
    latitudes, multiplied by the cosine of the declination. This number is
    passed to linspace, also triggering an error. This number is now
    converted to an integer before calling linspace.

--- spherical_geometry/tests/test_util.py
+++ spherical_geometry/tests/test_util.py
@@ -8,7 +8,8 @@ ROOT_DIR = os.path.join(os.path.dirname(__file__), 'data')
 def get_point_set(density=25):
     points = []
     for i in np.linspace(-85, 85, density, True):
-        for j in np.linspace(-180, 180, np.cos(np.deg2rad(i)) * density):
+        adjusted_density = int(np.cos(np.deg2rad(i)) * density)
+        for j in np.linspace(-180, 180, adjusted_density):
             points.append([j, i])
     points = np.asarray(points)
     return np.dstack(vector.radec_to_vector(points[:,0], points[:,1]))[0]
