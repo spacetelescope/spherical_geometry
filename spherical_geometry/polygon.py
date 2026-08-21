364158f251ebb2ff68d7246f54befa807c411bfd
Mihai Cara <mihail.cara@gmail.com>
2026-08-21 15:58:05 -0400

    hoping to improve loss of precision in area

--- spherical_geometry/polygon.py
+++ spherical_geometry/polygon.py
@@ -18,6 +18,12 @@ __all__ = ['SingleSphericalPolygon', 'SphericalPolygon',
            'MalformedPolygonError']
 
 
+if hasattr(np, 'float128'):
+    EXTENDED_FLT_TYPE = np.float128
+else:
+    EXTENDED_FLT_TYPE = np.longdouble
+
+
 class MalformedPolygonError(Exception):
     pass
 
@@ -717,7 +723,7 @@ class SingleSphericalPolygon(object):
         points = np.vstack((self._points, self._points[1]))
         angles = great_circle_arc.angle(points[:-2], points[1:-1], points[2:])
 
-        return np.sum(angles) - (len(angles) - 2) * np.pi
+        return float(np.sum(angles, dtype=EXTENDED_FLT_TYPE) - (len(angles) - 2) * np.pi)
 
     def union(self, other):
         """
